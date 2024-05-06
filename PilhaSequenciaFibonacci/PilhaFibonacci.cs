a7639717d479e8e4cf58ebd476ee558bf23a232d
Daiane Costa <cicuto.adv@gmail.com>
2024-05-06 04:19:48 -0300

    PilhaFobonacci

---
+++ PilhaSequenciaFibonacci/PilhaFibonacci.cs
@@ -0,0 +1,26 @@
+﻿class PilhaFibonacci
+{
+    public void ImprimirSequencia(int posicao)
+    {
+        Stack<int> pilha = new Stack<int>();
+        pilha.Push(0);
+        pilha.Push(1);
+
+        Console.WriteLine("Sequência de Fibonacci até a posição " + posicao + ":");
+
+        if (posicao >= 1) Console.WriteLine(0);
+        if (posicao >= 2) Console.WriteLine(1);
+
+        int count = 2;
+        while (count < posicao)
+        {
+            int penultimo = pilha.Pop();
+            int ultimo = pilha.Pop();
+            int atual = penultimo + ultimo;
+            pilha.Push(ultimo);
+            pilha.Push(atual);
+            Console.WriteLine(atual);
+            count++;
+        }
+    }
+}
--- PilhaSequenciaFibonacci/Program.cs
+++ PilhaSequenciaFibonacci/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int posicao;
+        Console.WriteLine("Digite a posição desejada na sequência de Fibonacci:");
+        posicao = int.Parse(Console.ReadLine());
+
+        if (posicao < 1)
+        {
+            Console.WriteLine("Por favor, insira uma posição maior ou igual a 1.");
+            return;
+        }
+
+        PilhaFibonacci pilhaFibonacci = new PilhaFibonacci();
+        pilhaFibonacci.ImprimirSequencia(posicao);
+    }
+}
